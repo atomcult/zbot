3172f587f005991c06669c85db586887228f2921
regulardeluxe <joseph.brock@protonmail.com>
2018-03-06 19:41:09 -0500

    Initial commit

---
+++ build.rs
@@ -0,0 +1,6 @@
+use std::process::Command;
+fn main() {
+    let output = Command::new("git").args(&["describe", "HEAD", "--tags"]).output().unwrap();
+    let git_version = String::from_utf8(output.stdout).unwrap();
+    println!("cargo:rustc-env=GIT_VERSION={}", git_version);
+}
