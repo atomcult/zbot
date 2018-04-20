65facc194b77388acb23cad7a515bce6298a1566
regulardeluxe <joseph.brock@protonmail.com>
2018-04-20 05:15:13 -0400

    Print error message when config.toml doesn't exist

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,4 @@
+use std::{env,process};
 use std::fs::File;
 use std::io::prelude::*;
 use toml;
@@ -10,7 +11,18 @@ pub struct Config {
 
 impl Config {
     pub fn open() -> Config {
-        let mut file = File::open("config.toml").unwrap();
+        // Open config.toml
+        let mut file = File::open("config.toml").unwrap_or_else( |_| {
+            if let Ok(dir) = env::current_dir() {
+                println!("`{}/config.toml` does not exist.", dir.to_str().unwrap());
+            } else {
+                println!("Current directory does not exist, or you do not have sufficient permissions.");
+            }
+
+            process::exit(1);
+        });
+
+        // Read contents of config.toml and serialize
         let mut contents = String::new();
         file.read_to_string(&mut contents).unwrap();
         toml::from_str(&contents).unwrap()
